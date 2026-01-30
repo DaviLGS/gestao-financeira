ae855b9eb1ba7982d115cc583dd73497e504ef64
Davi_LGS <davilessagarcia@gmail.com>
2026-01-30 02:05:54 -0300

    Commit 02 - atualizacao pom e controller

---
+++ src/test/java/financeira/gestao/demo/GestaoFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package financeira.gestao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
